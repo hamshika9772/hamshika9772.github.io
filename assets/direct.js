95f26d1ed8631e26d6c1eccf1685b88e5852fce6
Tharun9772Gaming <tharun2015gamer@gmail.com>
2026-06-26 17:37:43 -0400

    Fix formatting in direct.js

--- assets/direct.js
+++ assets/direct.js
@@ -1,5 +1,5 @@
 (function() {
-const _0x4f22 = ["\x64\x65\x62\x75\x67", "\x69\x6E\x69\x74"];
+const _0x4f22 = ["\x64\x65\x62\x75\x67", "\x69\x6E\x69\x74"]; 
 const UI_THEME_MODE_DEFAULT = 'light';
 const UI_THEME_MODE_DARK = 'dark';
 const UI_THEME_MODE_AUTO = 'system';
