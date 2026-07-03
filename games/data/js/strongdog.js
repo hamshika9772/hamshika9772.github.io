f2be94a133d22290c4a4f40a9b7b228db2000b03
Tharun9772Gaming <tharun2015gamer@gmail.com>
2026-07-02 21:51:14 -0400

    Update strongdog.js

--- games/data/js/strongdog.js
+++ games/data/js/strongdog.js
@@ -1,3 +1,4 @@
+// IMG base is https://strongdog.com/img/{$imgSrc}
 export default [
   { href: 'Human-or-not', imgSrc: 'human or not.png', name: 'Human or not', 'page': 1, 'id': 53429, og: true },
   // { href: 'lolbeans.io', imgSrc: 'lolbeans.jpeg', name: 'Lolbeans.io', 'page': 1, 'id': 53430, og: true },
