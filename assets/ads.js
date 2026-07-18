93a088cb90458a098293d304e2cd672271a4cc20
Tharun9772Gaming <tharun2015gamer@gmail.com>
2026-07-18 19:16:51 -0400

    Update ads.js

--- assets/ads.js
+++ assets/ads.js
@@ -4,8 +4,7 @@
         return; 
     } 
     var newScript = document.createElement('script'); 
-    newScript.src = 'https://effectivecpmnetwork.com'; 
-    newScript.async = true; 
+    newScript.src = 'https://pl30426901.effectivecpmnetwork.com/c7/09/bd/c709bd80ae0a08ba1e460d88205ea50a.js'; 
 
     var targetScript = document.querySelector('script[src="/assets/ads.js"]'); 
 
