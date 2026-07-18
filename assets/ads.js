1677c1de616f64454ab9bf0f5b363d2d419add9a
Tharun9772Gaming <tharun2015gamer@gmail.com>
2026-07-18 15:58:49 -0400

    Create ads.js

---
+++ assets/ads.js
@@ -0,0 +1,19 @@
+    (function() {
+    var adsKey = localStorage.getItem("bloxcraftstudiosadskey");
+    if (adsKey === "false") {
+        return; 
+    }
+
+    var newScript = document.createElement('script');
+    newScript.src = 'https://pl30426901.effectivecpmnetwork.com/c7/09/bd/c709bd80ae0a08ba1e460d88205ea50a.js';
+    newScript.async = true;
+
+    var targetScript = document.querySelector('script[src="/assets/ads.js"]');
+
+    if (targetScript && targetScript.parentNode) {
+        targetScript.parentNode.insertBefore(newScript, targetScript.nextSibling);
+    } else {
+   
+        document.body.appendChild(newScript);
+    }
+})();
