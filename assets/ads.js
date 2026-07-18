5bda0709077053a3ab2efda7c3e0bd019884f011
Tharun9772Gaming <tharun2015gamer@gmail.com>
2026-07-18 19:02:43 -0400

    Update ads.js

--- assets/ads.js
+++ assets/ads.js
@@ -1,9 +1,17 @@
-(function() {
-    var adsKey = localStorage.getItem("bloxcraftstudiosadskey");
-    if (adsKey === "false") {
-        var targetScript = document.querySelector('script[src="https://pl30426901.effectivecpmnetwork.com/c7/09/bd/c709bd80ae0a08ba1e460d88205ea50a.js"]');
-        if (targetScript) {
-            targetScript.remove();
-        }
+(function() { 
+    var adsKey = localStorage.getItem("bloxcraftstudiosadskey"); 
+    if (adsKey === "false") { 
+        return; 
+    } 
+    var newScript = document.createElement('script'); 
+    newScript.src = 'https://effectivecpmnetwork.com'; 
+    newScript.async = true; 
+
+    var targetScript = document.querySelector('script[src="/assets/ads.js"]'); 
+
+    if (targetScript && targetScript.parentNode) { 
+        targetScript.parentNode.insertBefore(newScript, targetScript.nextSibling); 
+    } else { 
+        document.body.appendChild(newScript); 
     }
 })();
